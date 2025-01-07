12388b3898857b9c77533f0ddcb78beb19438ee3
Chaimaa Chouhaibi <chaimaachouhaibi@gmail.com>
2025-01-07 14:44:49 +0100

    variables

---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+   int var1 = 1; //Statement 
+   int var2 ;
+   var2 = 3; //initialization
+   printf("var1= %d\nvar2= %d",var1,var2); //Format Specifier
+   return 0;
+}
